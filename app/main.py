eaf0975252e15c5bc87641cc6d661d175f9bc40a
Curtis St Pierre <cstpierre@xmatters.com>
2019-07-11 11:34:07 -0700

    get list of possible moves, optimize last move

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import random
 
 board_width = None
 board_height = None
+last_move = None
 
 @bottle.route('/static/<path:path>')
 def static(path):
@@ -41,7 +42,7 @@ def move():
     next_move = findFood(data)
     if next_move is None:
         next_move = findSafePlace(data)
-
+    print next_move
     return {
         'move': next_move,
         'taunt': 'Come get some!'
@@ -54,28 +55,50 @@ def getSnake(gameState, id):
             return snake
 
 
-def avoidCollision(snake, move):
-    head_x, head_y = snake["coords"][0]
-    if move is "left":
-        # x coord
+def availableMoves(gameState):
+    options = ['left', 'right', 'up', 'down']
+    snakes = gameState["snakes"]
+    for snake in snakes:
+        head_x, head_y = snake["coords"][0]
         new_head = [head_x - 1, head_y]
         if 0 > head_x - 1 or new_head in snake["coords"]:
-            return False
-    if move is "right":
-        # x coord
+            options.remove('left')
         new_head = [head_x + 1, head_y]
         if board_width <= head_x + 1 or new_head in snake["coords"]:
-            return False
-    if move is "up":
-        # x coord
+            options.remove('right')
         new_head = [head_x, head_y - 1]
         if 0 > head_y - 1 or new_head in snake["coords"]:
-            return False
-    if move is "down":
-        # x coord
+            options.remove('up')
         new_head = [head_x, head_y + 1]
         if board_height <= head_y + 1 or new_head in snake["coords"]:
-            return False
+            options.remove('down')
+        print options
+    return options
+
+
+def avoidCollision(snakes, move):
+    for snake in snakes:
+        head_x, head_y = snake["coords"][0]
+        if move is "left":
+            # x coord
+            new_head = [head_x - 1, head_y]
+            if 0 > head_x - 1 or new_head in snake["coords"]:
+                return False
+        if move is "right":
+            # x coord
+            new_head = [head_x + 1, head_y]
+            if board_width <= head_x + 1 or new_head in snake["coords"]:
+                return False
+        if move is "up":
+            # x coord
+            new_head = [head_x, head_y - 1]
+            if 0 > head_y - 1 or new_head in snake["coords"]:
+                return False
+        if move is "down":
+            # x coord
+            new_head = [head_x, head_y + 1]
+            if board_height <= head_y + 1 or new_head in snake["coords"]:
+                return False
     return True
 
 
@@ -84,19 +107,26 @@ def findFood(gameState):
     mySnake = getSnake(gameState, gameState["you"])
     head = mySnake["coords"][0]
 
+    available_moves = availableMoves(gameState)
     move = None
+    print available_moves
+    if last_move and gameState["food"][0][0] < head[0] and last_move in available_moves:
+            move = last_move
 
-    if gameState["food"][0][0] < head[0] and avoidCollision(mySnake, "left"):
+    if gameState["food"][0][0] < head[0] and "left" in available_moves:
         move = "left"
 
-    if gameState["food"][0][0] > head[0] and avoidCollision(mySnake, "right"):
+    if gameState["food"][0][0] > head[0] and "right" in available_moves:
         move = "right"
 
-    if gameState["food"][0][1] < head[1] and avoidCollision(mySnake, "up"):
+    if gameState["food"][0][1] < head[1] and "up" in available_moves:
         move = "up"
 
-    if gameState["food"][0][1] > head[1] and avoidCollision(mySnake, "down"):
+    if gameState["food"][0][1] > head[1] and "down" in available_moves:
         move = "down"
+
+    global last_move
+    last_move = move
     return move
 
 
@@ -105,16 +135,16 @@ def findSafePlace(gameState):
     mySnake = getSnake(gameState, gameState["you"])
     head = mySnake["coords"][0]
     move = None
-    if avoidCollision(mySnake, "left"):
+    if avoidCollision(gameState["snakes"], "left"):
         move = "left"
 
-    if avoidCollision(mySnake, "right"):
+    if avoidCollision(gameState["snakes"], "right"):
         move = "right"
 
-    if avoidCollision(mySnake, "up"):
+    if avoidCollision(gameState["snakes"], "up"):
         move = "up"
 
-    if avoidCollision(mySnake, "down"):
+    if avoidCollision(gameState["snakes"], "down"):
         move = "down"
     return move
 
